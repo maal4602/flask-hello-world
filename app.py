cfa34d308aa41172c27c2cfef590f0db85a3f15d
maal4602 <maal4602@colorado.edu>
2023-03-29 19:44:13 +0000

    Create db on render

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
+import psycopg2
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+@app.route('/db_test')
+def testing():
+    conn = psycopg2.connect("postgres://cspb3308_lab10_db_user:PlUaHbOmC7ShaAVxj9fG1pxMHAdTM7Y8@dpg-cgi9711r8t1g7lvn4ghg-a/cspb3308_lab10_db")
+    conn.close()
+    return "Database Connection Successful"
